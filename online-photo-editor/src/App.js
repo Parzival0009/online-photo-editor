235a7173f8c5e493a5c21a1abf50854c60cfecbb
Prathamesh Patil <prathameshp@thoughtsketch.com>
2024-01-04 10:20:46 +0000

    first-commit

---
+++ online-photo-editor/src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import ImageEditor from "./components/ImageEditor";
+
+class App extends Component {
+  render() {
+    return <ImageEditor />;
+  }
+}
+export default App;
